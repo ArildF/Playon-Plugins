d51d1aca2b71a5bfc3d4fe3e23e34e10345962b8
Arild <arild.fines@broadpark.no>
2010-09-23 23:58:42 +0200

    Even moar coverages

---
+++ src/Channel9Plugin.Tests/Integration/Settings.feature
@@ -0,0 +1,27 @@
+﻿Feature: Settings
+    In order to configure the Channel 9 MSDN plugin
+    As an administrator
+    I want to access settings
+
+Background:
+    Given a settings object
+
+
+Scenario: Properties
+    Then the settings should have a description of 'Channel 9 (MSDN)'
+    And the settings should have an image
+    And the settings should have an ID
+
+Scenario: Link
+    Then the settings should have the link 'http://github.com/ArildF/Channel-9-Playon-Plugin' 
+
+Scenario: Name
+    Then the settings should have the name 'Channel 9 (MSDN)'
+
+Scenario: Unsupported features
+    Then checking for updates should not be supported
+    And configuring options should not be supported
+
+Scenario: Should not require login
+    Then login should not be required
+
---
+++ src/Channel9Plugin.Tests/Integration/Steps/SettingsSteps.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.XPath;
+using MediaMallTechnologies.Plugin;
+using Moq;
+using NUnit.Framework;
+using Rogue.PlayOn.Plugins.Channel9;
+using TechTalk.SpecFlow;
+using Satisfyr;
+
+// ReSharper disable InconsistentNaming
+namespace Channel9Plugin.Tests.Integration.Steps
+{
+    [Binding]
+    public class SettingsSteps : TestBase
+    {
+        private string _rss;
+        private Channel9Settings _settings;
+
+        [BeforeScenario]
+        public void Init()
+        {
+            base.Setup();
+        }
+
+
+        [Given(@"a settings object")]
+        public void GivenASettingsObject()
+        {
+            _settings = new Channel9Settings();
+
+        }
+
+
+        [Then(@"the settings should have a description of '(.*)'")]
+        public void ThenTheSettingsShouldHaveADescriptionOfChannel9MSDN(string desc)
+        {
+            _settings.Satisfies(s => s.Description == desc);
+        }
+
+        [Then(@"the settings should have an image")]
+        public void ThenTheSettingsShouldHaveAnImage()
+        {
+            _settings.Satisfies(s => s.Image != null);
+        }
+
+        [Then(@"the settings should have an ID")]
+        public void ThenTheSettingsShouldHaveAnID()
+        {
+            _settings.Satisfies(s => s.ID != null);
+        }
+
+        [Then(@"the settings should have the name '(.*)'")]
+        public void ThenTheSettingsShouldHaveTheNameChannel9MSDN(string name)
+        {
+            _settings.Satisfies(s => s.Name == name);
+        }
+
+        [Then(@"the settings should have the link '(.*)'")]
+        public void ThenTheSettingsShouldHaveTheLinkHttpGithub_ComArildFChannel_9_Playon_Plugin(string url)
+        {
+            _settings.Link.Satisfies(lnk => lnk == url);
+        }
+
+        [Then(@"checking for updates should not be supported")]
+        public void ThenCheckingForUpdatesShouldNotBeSupported()
+        {
+            _settings.CheckForUpdate().Satisfies(u => u == null);
+        }
+
+        [Then(@"configuring options should not be supported")]
+        public void ThenConfiguringOptionsShouldNotBeSupported()
+        {
+            _settings.HasOptions.Satisfies(ho => ho == false);
+            Assert.Throws<NotSupportedException>(() => _settings.ConfigureOptions(null, null));
+        }
+
+        [Then(@"login should not be required")]
+        public void ThenLoginShouldNotBeRequired()
+        {
+            _settings.RequiresLogin.Satisfies(rl => rl == false);
+            Assert.Throws<NotSupportedException>(() => _settings.TestLogin(null, null));
+        }
+
+    }
+}
+
+// ReSharper restore InconsistentNaming
